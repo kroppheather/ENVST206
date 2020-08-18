f737314c4e8b0dfb5e145eb12b3a16acbadc724f
kroppheather <kroppheather@gmail.com>
2020-08-18 11:59:46 -0400

    added example files

---
+++ code/simple_script.r
@@ -0,0 +1,18 @@
+# Activity 1
+print("hello R world")
+
+#remember this is a comment so it won't do anything. R just ignores it.
+#you need to use these to document code and write notes
+#6 raised to the 6 power
+6^6
+# 5 plus 210
+5+210
+#3 minus 10
+3-10
+
+#name my number
+a <- 2446
+#multiply my number by 5
+5*a
+#divide my number by 2
+a/2
